bc6e1d65e8aacae93f76c65cbd2abc4ee1cfa019
tiago <tiagolpadua@gmail.com>
2018-11-03 22:30:07 -0300

    jsx

--- App.js
+++ App.js
@@ -1,36 +1,31 @@
 import { Constants } from 'expo';
 import * as React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { clicks: 0 };
-    console.log('O componente foi construído.');
   }
 
-  componentDidMount() {
-    console.log('O componente foi montado.');
-  }
-
-  componentDidUpdate() {
-    console.log('O componente foi atualizado.');
-  }
-
-  componentWillUnmount() {
-    console.log('O componente será desmontado.');
-  }
-
-  handleClick = () => {
-    this.setState(prevState => ({ clicks: prevState.clicks + 1 }));
+  formatMessage(message = '') {
+    let el;
+    if(message.length > 2) {
+      el = <Text>{message.toUpperCase()}</Text>;
+    } else {
+      el = <Text>{message}</Text>;
+    }
+    return el;
   }
 
   render() {
-    console.log('Render foi acionado.');
+    const nome = 'José';
+    const saudacao = 'oi';
+
     return (
       <View style={styles.container}>
-        <Button title={`Clicou ${this.state.clicks} vezes`} onPress={this.handleClick}></Button>
+        {this.formatMessage(saudacao)}
+        {this.formatMessage(nome)}
       </View>
     );
   }
